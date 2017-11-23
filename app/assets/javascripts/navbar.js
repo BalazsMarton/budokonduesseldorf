388da451682e3d09a09ee960b0e6de69dc08c4ed
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-11-23 11:27:16 +0100

    collapsed topnavbar background

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -6,10 +6,16 @@ document.addEventListener("turbolinks:load", function() {
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
 	    if ($(".navbar").offset().top > 100) {
+	    	$(".navbar").removeClass("navontop,navbutton-active");
 	        $(".navbar").addClass("top-nav-collapse");
 	    } else {
 	        $(".navbar").removeClass("top-nav-collapse");
+	        $(".navbar").addClass("navontop");
 	    }
+	});
+
+	$( ".navbar-toggler" ).click(function() {
+  		$('.navontop').toggleClass('navbutton-active');
 	});
 
 });
