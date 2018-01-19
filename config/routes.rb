ddb029f9861aa3767ed29c5fe0a28dd81a3bfac2
BalazsMarton <balazsmarton90.joomla@gmail.com>
2018-01-19 20:52:49 +0100

    admin sign out button, admins to admin route

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  devise_for :admins, skip: :registrations
+  devise_for :admins, path: 'admin', skip: :registrations
   #scope module: 'admin', path: 'admin' do
   namespace :admin do
     root 'pages#adminpaneldemo'
