8791f00facdb640ae712c69c61ca5c9e571e6dcf
BalazsMarton <balazsmarton90.joomla@gmail.com>
2018-11-05 12:28:01 +0100

    uplaods dir dev storage

--- app/uploaders/event_uploader.rb
+++ app/uploaders/event_uploader.rb
@@ -8,8 +8,11 @@ class EventUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  storage :fog
-
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,19 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-	config.fog_provider = 'fog/aws' 
-	config.fog_credentials = {
-		:provider              => 'AWS',
-		:aws_access_key_id     => ENV['S3_KEY'],
-	    :aws_secret_access_key => ENV['S3_SECRET'],
-	    :region                => ENV['S3_REGION']
-	}
 
-	config.fog_directory =  ENV['S3_BUCKET_NAME']
-	config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+	if Rails.env.development? || Rails.env.test?
+    	config.storage = :file
+	else
+		config.fog_provider = 'fog/aws' 
+		config.fog_credentials = {
+			:provider              => 'AWS',
+			:aws_access_key_id     => ENV['S3_KEY'],
+		    :aws_secret_access_key => ENV['S3_SECRET'],
+		    :region                => ENV['S3_REGION']
+		}
+
+		config.fog_directory =  ENV['S3_BUCKET_NAME']
+		config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+	end
 end
