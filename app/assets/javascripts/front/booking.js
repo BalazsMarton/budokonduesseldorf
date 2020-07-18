3a184a302e458c4960294ded177c34de4e3c28c8
Tamas Mohos <zerosuxx@gmail.com>
2020-07-18 23:31:13 +0200

    adds booking.js

---
+++ app/assets/javascripts/front/booking.js
@@ -0,0 +1,80 @@
+document.addEventListener("turbolinks:load", async function() {
+    if (app.dataset.controller === 'pages' && app.dataset.action === 'booking') {
+        await initBooking();
+    }
+});
+
+async function initBooking() {
+    const bookingService = new BookingService(new BookingClient());
+    await bookingService.login();
+
+    console.log(await bookingService.getEvents());
+    console.log(await bookingService.getAvailableTimes('2020-07-20'));
+}
+
+class BookingClient {
+    async getToken() {
+        return this._client('/booking/token')
+            .then(data => Promise.resolve(data.token));
+    }
+
+    async getEvents(token) {
+        return this._client('/booking/events', {
+            headers: {
+                'X-Auth-Token': token
+            }
+        });
+    }
+
+    async getAvailableTimes(token, date) {
+        return this._client(`/booking/available-times/${date}`, {
+            headers: {
+                'X-Auth-Token': token
+            }
+        });
+    }
+
+    async book(token) {
+        return this._client(`/booking/add`, {
+            method: 'POST',
+            headers: {
+                'X-Auth-Token': token
+            }
+        });
+    }
+
+    /**
+     *
+     * @param {Request|string} url
+     * @param {RequestInit} initOptions
+     * @returns {Promise<any>}
+     * @private
+     */
+    async _client(url, initOptions = {}) {
+        return fetch(url, initOptions)
+            .then(response => response.json());
+    }
+}
+
+class BookingService {
+    _token = null;
+
+    /**
+     * @param {BookingClient} bookingClient
+     */
+    constructor(bookingClient) {
+        this._bookingClient = bookingClient;
+    }
+
+    async login() {
+        this._token = await this._bookingClient.getToken();
+    }
+
+    async getEvents() {
+        await this._bookingClient.getEvents(this._token);
+    }
+
+    async getAvailableTimes(date) {
+        await this._bookingClient.getAvailableTimes(this._token, date);
+    }
+}
